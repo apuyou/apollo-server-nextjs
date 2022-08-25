d5d2bbe72e3adec64c1de7905e990ceff16c85a4
Arthur Puyou <arthur@puyou.fr>
2022-08-26 00:47:54 +0200

    Create an integration for Apollo 4

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,99 @@
-export {
-  GraphQLOptions,
-  Config,
-  gql,
-  // Errors
-  ApolloError,
-  toApolloError,
-  SyntaxError,
-  ValidationError,
-  AuthenticationError,
-  ForbiddenError,
-  UserInputError,
-} from 'apollo-server-core';
+import type { NextApiRequest, NextApiResponse, NextApiHandler } from "next";
+import type {
+  ApolloServer,
+  BaseContext,
+  ContextFunction,
+  HTTPGraphQLRequest,
+} from "@apollo/server";
+import type { WithRequired } from "@apollo/utils.withrequired";
 
-// ApolloServer integration.
-export { ApolloServer, CreateHandlerOptions } from './ApolloServer';
+import { parse as urlParse } from "url";
+
+export interface NextContextFunctionArgument {
+  req: NextApiRequest;
+  res: NextApiResponse;
+}
+
+export interface NextHandlerOptions<TContext extends BaseContext> {
+  context?: ContextFunction<[NextContextFunctionArgument], TContext>;
+}
+
+export function nextHandler(
+  server: ApolloServer<BaseContext>,
+  options?: NextHandlerOptions<BaseContext>
+): NextApiHandler;
+export function nextHandler<TContext extends BaseContext>(
+  server: ApolloServer<TContext>,
+  options: WithRequired<NextHandlerOptions<TContext>, "context">
+): NextApiHandler;
+export function nextHandler<TContext extends BaseContext>(
+  server: ApolloServer<TContext>,
+  options?: NextHandlerOptions<TContext>
+): NextApiHandler {
+  server.startInBackgroundHandlingStartupErrorsByLoggingAndFailingAllRequests();
+
+  // This `any` is safe because the overload above shows that context can
+  // only be left out if you're using BaseContext as your context, and {} is a
+  // valid BaseContext.
+  const defaultContext: ContextFunction<
+    [NextContextFunctionArgument],
+    any
+  > = async () => ({});
+
+  const contextFunction: ContextFunction<
+    [NextContextFunctionArgument],
+    TContext
+  > = options?.context ?? defaultContext;
+
+  return async function (req, res) {
+    const headers = new Map<string, string>();
+    for (const [key, value] of Object.entries(req.headers)) {
+      if (value !== undefined) {
+        // Node/Express headers can be an array or a single value. We join
+        // multi-valued headers with `, ` just like the Fetch API's `Headers`
+        // does. We assume that keys are already lower-cased (as per the Node
+        // docs on IncomingMessage.headers) and so we don't bother to lower-case
+        // them or combine across multiple keys that would lower-case to the
+        // same value.
+        headers.set(key, Array.isArray(value) ? value.join(", ") : value);
+      }
+    }
+
+    if (!req.method) {
+      res.status(500);
+      res.send("`req.method` is not set");
+      return;
+    }
+
+    const httpGraphQLRequest: HTTPGraphQLRequest = {
+      method: req.method.toUpperCase(),
+      headers,
+      search: urlParse(req.url || "").search ?? "",
+      body: req.body,
+    };
+
+    try {
+      const httpGraphQLResponse = await server.executeHTTPGraphQLRequest({
+        httpGraphQLRequest,
+        context: () => contextFunction({ req, res }),
+      });
+
+      if (httpGraphQLResponse.completeBody === null) {
+        throw Error("Incremental delivery not implemented");
+      }
+
+      res.status(httpGraphQLResponse.status || 200);
+
+      for (const key in httpGraphQLResponse.headers) {
+        const value = httpGraphQLResponse.headers.get(key);
+        if (value) {
+          res.setHeader(key, value);
+        }
+      }
+
+      res.send(httpGraphQLResponse.completeBody);
+    } catch (error) {
+      throw error;
+    }
+  };
+}
